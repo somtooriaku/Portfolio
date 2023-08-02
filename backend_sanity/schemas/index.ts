64332131f2334ef3ca6309ce64895a205e0594ad
FxT <59647845+somtooriaku@users.noreply.github.com>
2023-08-01 20:27:39 -0400

    Add files via upload

---
+++ backend_sanity/schemas/index.ts
@@ -0,0 +1,11 @@
+// schemas/index.js
+import abouts from './abouts'
+import testimonials from './testimonials'
+import brands from './brands'
+import contact from './contact'
+import experiences from './experiences'
+import works from './works'
+import skills from './skills'
+import workExperience from './workExperience'
+
+export const schemaTypes = [testimonials, abouts, brands, contact, experiences, works, skills, workExperience]
